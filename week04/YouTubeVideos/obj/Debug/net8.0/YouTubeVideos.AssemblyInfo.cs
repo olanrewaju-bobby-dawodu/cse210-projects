24d341ad9dd20d36be9e70dbaeb41f815702e4e9
olanrewaju-bobby-dawodu <olanrewaju-bobby-dawodu@users.noreply.github.com>
2025-06-12 16:18:15 +0100

    Program ScreenShots

--- week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
+++ week04/YouTubeVideos/obj/Debug/net8.0/YouTubeVideos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f308b3997a7cc170d8211c7f5156f8e1c1613343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807e758a057c82efaf8bdc0e8d41f3f5b9786feb")]
 [assembly: System.Reflection.AssemblyProductAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YouTubeVideos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
